ef9aa35a904b7e9dc3328bb6fc3b2f974ff8191c
Modesta Maya <modesta.maya@outlook.com>
2019-02-24 00:24:41 +0200

    Additional Intro Line to Print

--- bs.py
+++ bs.py
@@ -20,7 +20,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-
+    print('We have Chicago, Washington and New York City available for you to explore.')
+    
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     def city_choice():
         while True:
